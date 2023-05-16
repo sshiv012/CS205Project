b6cf80a4dd70cf0ce3123173c0f03995e83057b7
sshiv012 <sshiv012@ucr.edu>
2023-05-15 18:32:25 -0700

    Code reformatting and restructuring x2
    Code reformatting and restructuring x2

--- src/main/java/com/ucr/edu/cs205/project1/EamonnsGodAlgorithm.java
+++ src/main/java/com/ucr/edu/cs205/project1/EamonnsGodAlgorithm.java
@@ -7,10 +7,11 @@ import java.util.stream.Collectors;
 
 public class EamonnsGodAlgorithm {
 
-    public enum QueueFunction{ UCS,AStarMisplaced, AStarManhattan }
-    private static int boardEdge=0;
-    private static int boardSize=9;
-    private boolean isBacktrackingEnabled=false;
+    public enum QueueFunction {UCS, AStarMisplaced, AStarManhattan}
+
+    private static int boardEdge = 0;
+    private static int boardSize = 9;
+    private boolean isBacktrackingEnabled = false;
     private static final List<Integer> goalState = new ArrayList<>();
 
 
@@ -23,7 +24,8 @@ public class EamonnsGodAlgorithm {
             this.node = node;
         }
     }
-    public List<Node> expand(Node node){
+
+    public List<Node> expand(Node node) {
         List<Node> children = new ArrayList<>();
         int positionOfEmptyTile = node.getState().indexOf(0);
         Node child;
@@ -47,15 +49,15 @@ public class EamonnsGodAlgorithm {
 
         return children;
     }
-    public void ultimateSearch(Node initialState, QueueFunction queueFunction)
-    {
-        int noOfExpandedNodes=0;
-        int maxQSize=0;
-        if(isGoalState(initialState)) {
+
+    public void ultimateSearch(Node initialState, QueueFunction queueFunction) {
+        int noOfExpandedNodes = 0;
+        int maxQSize = 0;
+        if (isGoalState(initialState)) {
             System.out.println("The puzzle is already solved");
             return;
         }
-        Comparator<NodeCost> comparator = (o1, o2) -> Float.compare(o1.cost,o2.cost);
+        Comparator<NodeCost> comparator = (o1, o2) -> Float.compare(o1.cost, o2.cost);
         PriorityQueue<NodeCost> nodes = new PriorityQueue<>(100, comparator);
         Map<String, Boolean> visited = new HashMap<>();
         nodes.add(new NodeCost(Float.POSITIVE_INFINITY, initialState));
@@ -67,90 +69,87 @@ public class EamonnsGodAlgorithm {
             maxQSize = Math.max(maxQSize, nodes.size());
             NodeCost nodeCost = nodes.poll();
 
-            if(isBacktrackingEnabled) {
+            if (isBacktrackingEnabled) {
                 if (queueFunction.equals(QueueFunction.UCS)) {
-                    System.out.println("The best state to expand with a g(n) = " + nodeCost.node.getDepth() + " and h(n) = " + 0+ " is .... ");
+                    System.out.println("The best state to expand with a g(n) = " + nodeCost.node.getDepth() + " and h(n) = " + 0 + " is .... ");
                 } else
-                    System.out.println("The best state to expand with a g(n) = " + nodeCost.node.getDepth() + " and h(n) = " + nodeCost.cost+ " is ....");
-                System.out.println("Depth: "+nodeCost.node.getDepth()+ " Node Cost: "+ nodeCost.cost);
-                for(int i=0;i<boardSize;i++)
-                {
-                    if(i!=0 && i%(boardEdge)==0)
+                    System.out.println("The best state to expand with a g(n) = " + nodeCost.node.getDepth() + " and h(n) = " + nodeCost.cost + " is ....");
+                System.out.println("Depth: " + nodeCost.node.getDepth() + " Node Cost: " + nodeCost.cost);
+                for (int i = 0; i < boardSize; i++) {
+                    if (i != 0 && i % (boardEdge) == 0)
                         System.out.println();
-                    System.out.print(nodeCost.node.getState().get(i)+" ");
+                    System.out.print(nodeCost.node.getState().get(i) + " ");
                 }
                 System.out.println();
             }
 
-            if(isGoalState(nodeCost.node)) {
-                System.out.println("Solution depth was "+ nodeCost.node.getDepth()+
+            if (isGoalState(nodeCost.node)) {
+                System.out.println("Solution depth was " + nodeCost.node.getDepth() +
                         "\nNumber of nodes expanded: " + noOfExpandedNodes +
                         "\nMax Queue Size: " + maxQSize);
 
                 Node test = nodeCost.node;
-                while(test!=null && isBacktrackingEnabled)
-                {
-                    System.out.println("Backtracking solution from Goal State to Initial State, State at Depth- "+test.getDepth());
-                    System.out.println("Depth: "+test.getDepth());
-                    for(int i=0;i<boardSize;i++)
-                    {
-                        if(i!=0 && i%(boardEdge)==0)
+                while (test != null && isBacktrackingEnabled) {
+                    System.out.println("Backtracking solution from Goal State to Initial State, State at Depth- " + test.getDepth());
+                    System.out.println("Depth: " + test.getDepth());
+                    for (int i = 0; i < boardSize; i++) {
+                        if (i != 0 && i % (boardEdge) == 0)
                             System.out.println();
-                        System.out.print(test.getState().get(i)+" ");
+                        System.out.print(test.getState().get(i) + " ");
                     }
                     System.out.println();
-                     test = test.getParentNode();
+                    test = test.getParentNode();
                 }
                 return;
             }
             visited.put(getListOfNumbersAsString(nodeCost.node.getState()), true);
-            for(Node child: expand(nodeCost.node))
-            {
-                if(!visited.containsKey(getListOfNumbersAsString(child.getState()))){
-                    if(queueFunction.equals(QueueFunction.UCS))
+            for (Node child : expand(nodeCost.node)) {
+                if (!visited.containsKey(getListOfNumbersAsString(child.getState()))) {
+                    if (queueFunction.equals(QueueFunction.UCS))
                         nodes.add(new NodeCost(child.getDepth(), child));
-                    else if(queueFunction.equals(QueueFunction.AStarMisplaced))
+                    else if (queueFunction.equals(QueueFunction.AStarMisplaced))
                         nodes.add(new NodeCost(child.getDepth() + calculateMisplacedTilesCount(child.getState()), child));
                     else
                         nodes.add(new NodeCost(child.getDepth() + calculateTotalManhattanDistance(child.getState()), child));
                 }
             }
-            noOfExpandedNodes+=1;
+            noOfExpandedNodes += 1;
         }
 
 
     }
 
-    public static String getListOfNumbersAsString(List<Integer> intList)
-    {
-        if(intList==null||intList.isEmpty())
+    public static String getListOfNumbersAsString(List<Integer> intList) {
+        if (intList == null || intList.isEmpty())
             return "";
         return intList.stream().map(String::valueOf).collect(Collectors.joining());
     }
-    public static boolean checkPerfectSquare(double number)
-    {
-        double sqrt=Math.sqrt(number);
+
+    public static boolean checkPerfectSquare(double number) {
+        double sqrt = Math.sqrt(number);
         return ((sqrt - Math.floor(sqrt)) == 0);
     }
+
     public int calculateMisplacedTilesCount(List<Integer> gameState) {
         int misplacedCount = 0;
-        for(int i = 0; i < boardSize; i++) {
-            if(gameState.get(i) == 0) {
+        for (int i = 0; i < boardSize; i++) {
+            if (gameState.get(i) == 0) {
                 continue;
             }
-            if(!gameState.get(i).equals(goalState.get(i))) {
+            if (!gameState.get(i).equals(goalState.get(i))) {
                 misplacedCount++;
             }
         }
         return misplacedCount;
     }
+
     public int calculateTotalManhattanDistance(List<Integer> gameState) {
         int totalDistance = 0;
         for (int i = 1; i < boardSize; i++) {
             int currentIndex = gameState.indexOf(i);
             int goalIndex = goalState.indexOf(i);
 
-            if(currentIndex == goalIndex) {
+            if (currentIndex == goalIndex) {
                 continue;
             }
             int currentRow = (currentIndex / boardEdge) + 1;
@@ -164,24 +163,24 @@ public class EamonnsGodAlgorithm {
         return totalDistance;
     }
 
-    public Boolean isGoalState(Node currState){
+    public Boolean isGoalState(Node currState) {
         return goalState.equals(currState.getState());
     }
 
     public void generateGoalState() {
-        for(int i = 1; i < boardSize; i++)
+        for (int i = 1; i < boardSize; i++)
             goalState.add(i);
         goalState.add(0);
     }
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to an 8-Puzzle Solver. Type '1' to use a default puzzle, or '2' to create your own");
         int choice1 = sc.nextInt();
         int choice2;
         int choice3;
         QueueFunction algoChoice = QueueFunction.AStarManhattan;
-        List<Integer>inputPuzzle = new ArrayList<>();
+        List<Integer> inputPuzzle = new ArrayList<>();
         Map<Integer, List<Integer>> eightPuzzle = new HashMap<>();
 
         eightPuzzle.put(0, Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 0)); // 0 - trivial
@@ -191,33 +190,29 @@ public class EamonnsGodAlgorithm {
         eightPuzzle.put(4, Arrays.asList(8, 7, 1, 6, 0, 2, 5, 4, 3)); // 4 - oh boy
         eightPuzzle.put(5, Arrays.asList(1, 2, 3, 4, 5, 6, 8, 7, 0)); // 5 - impossible
 
-        if(!checkPerfectSquare(boardSize)) {
+        if (!checkPerfectSquare(boardSize)) {
             System.out.println("Please intialize a board size that is a perfect square");
             return;
         }
-        boardEdge=(int) Math.sqrt(boardSize);
-        if(choice1==2)
-        {   System.out.println("Enter the size of the puzzle: ");
-            boardSize=sc.nextInt();
-            if(!checkPerfectSquare(boardSize)) {
+        boardEdge = (int) Math.sqrt(boardSize);
+        if (choice1 == 2) {
+            System.out.println("Enter the size of the puzzle: ");
+            boardSize = sc.nextInt();
+            if (!checkPerfectSquare(boardSize)) {
                 System.out.println("Please intialize a board size that is a perfect square");
                 return;
             }
-            boardEdge=(int) Math.sqrt(boardSize);
+            boardEdge = (int) Math.sqrt(boardSize);
             System.out.println("Enter the puzzle in the order asked");
-            for(int i=0;i<boardEdge;i++)
-            {
-                System.out.println("Enter the values for row "+(i+1));
-                for(int j=0; j<boardEdge; j++)
-                {
+            for (int i = 0; i < boardEdge; i++) {
+                System.out.println("Enter the values for row " + (i + 1));
+                for (int j = 0; j < boardEdge; j++) {
                     int x = sc.nextInt();
-                    if(inputPuzzle.contains(x))
-                    {
+                    if (inputPuzzle.contains(x)) {
                         System.out.println("Duplicate value entered, Please try again");
                         return;
                     }
-                    if(x<0||x>boardSize-1)
-                    {
+                    if (x < 0 || x > boardSize - 1) {
                         System.out.println("Duplicate value entered, Please try again");
                         return;
                     }
@@ -226,40 +221,35 @@ public class EamonnsGodAlgorithm {
 
             }
 
-        }
-        else
-        {
+        } else {
             System.out.println("Select difficulty for 8 puzzle problem on the scale of 0-5, 0 being trivial and 5 being impossible");
-            choice2= sc.nextInt();
-            inputPuzzle=eightPuzzle.get(choice2);
-            if(choice2<0||choice2>5) {
+            choice2 = sc.nextInt();
+            inputPuzzle = eightPuzzle.get(choice2);
+            if (choice2 < 0 || choice2 > 5) {
                 System.out.println("Please enter a right choice, try running once again");
                 return;
             }
         }
 
         System.out.println("Select algorithm. (1) for Uniform Cost Search, (2) for A* with Misplaced Tile Heuristic, or (3) for A* with Manhattan Distance Heuristic");
-        switch(sc.nextInt())
-        {
-            case 1 :
-            {
-                algoChoice=QueueFunction.UCS;
+        switch (sc.nextInt()) {
+            case 1: {
+                algoChoice = QueueFunction.UCS;
                 break;
             }
 
-            case 2 :
-            {
-                algoChoice=QueueFunction.AStarMisplaced;
+            case 2: {
+                algoChoice = QueueFunction.AStarMisplaced;
                 break;
             }
         }
 
         System.out.println("Do you want to see back tracking? (1) Yes or (2) No");
-        choice3= sc.nextInt();
+        choice3 = sc.nextInt();
         EamonnsGodAlgorithm search = new EamonnsGodAlgorithm();
 
-        if(choice3==1)
-            search.isBacktrackingEnabled=true;
+        if (choice3 == 1)
+            search.isBacktrackingEnabled = true;
 
         Node testGame = new Node(inputPuzzle, null);
         search.generateGoalState();
--- src/main/java/com/ucr/edu/cs205/project1/Node.java
+++ src/main/java/com/ucr/edu/cs205/project1/Node.java
@@ -3,41 +3,41 @@ package com.ucr.edu.cs205.project1;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Node{
-        private final List<Integer> state;
+public class Node {
+    private final List<Integer> state;
 
-        private final Node parentNode;
+    private final Node parentNode;
 
-        private final int depth;
+    private final int depth;
 
 
-        public Node(List<Integer> state, Node parentNode) {
-            this.state = new ArrayList<>(state);
-            if(parentNode == null) {
-                this.parentNode = null;
-                this.depth = 0;
-            } else {
-                this.parentNode = parentNode;
-                this.depth = this.parentNode.depth + 1;
-            }
+    public Node(List<Integer> state, Node parentNode) {
+        this.state = new ArrayList<>(state);
+        if (parentNode == null) {
+            this.parentNode = null;
+            this.depth = 0;
+        } else {
+            this.parentNode = parentNode;
+            this.depth = this.parentNode.depth + 1;
         }
+    }
 
-        public Node getParentNode() {
-            return parentNode;
-        }
+    public Node getParentNode() {
+        return parentNode;
+    }
 
-        public int getDepth() {
-            return depth;
-        }
+    public int getDepth() {
+        return depth;
+    }
 
-        public List<Integer> getState(){
-            return this.state;
-        }
+    public List<Integer> getState() {
+        return this.state;
+    }
 
-        public void swapTiles(int pos1, int pos2) {
-            int temp = this.state.get(pos1);
-            this.state.set(pos1, this.state.get(pos2));
-            this.state.set(pos2, temp);
-        }
+    public void swapTiles(int pos1, int pos2) {
+        int temp = this.state.get(pos1);
+        this.state.set(pos1, this.state.get(pos2));
+        this.state.set(pos2, temp);
+    }
 
 }
